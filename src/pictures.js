b9797bb7f6da54519108c197ef7a59ef0c3fb88d
Irina Mozes <irina5687@gmail.com>
2016-08-24 19:57:32 +0500

    module4-task2

--- src/pictures.js
+++ src/pictures.js
@@ -3,6 +3,48 @@
 var addr = 'http://localhost:1506/api/pictures?callback=__jsonpCallback';
 window.pictures = null;
 
+var picturesContainer = document.querySelector('.pictures');
+var templateElement = document.querySelector('template');
+var filtersBloc = document.querySelector('.filters');
+filtersBloc.classList.add('hidden');
+var elementToClone;
+
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.picture');
+} else {
+  elementToClone = templateElement.querySelector('.picture');
+}
+
+/** @constant {number} */
+var IMAGE_LOAD_TIMEOUT = 10000;
+
+/**
+ * @param {Object} data
+ * @param {HTMLElement} container
+ * @return {HTMLElement}
+ */
+var getPictureElement = function(data, container) {
+  var element = elementToClone.cloneNode(true);
+  container.appendChild(element);
+  var _picture = new Image();
+  var _pictureTimeout;
+  _picture.onload = function(evt) {
+    clearTimeout(_pictureTimeout);
+    element.querySelector('img').src = evt.target.src;
+    _picture.width = '182';
+    _picture.height = '182';
+  };
+  _picture.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
+  _picture.src = data.url;
+  _pictureTimeout = setTimeout(function() {
+    _picture.src = '';
+    element.classList.add('picture-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+  return element;
+};
+
 var loadAndCreate = function(addres, callback2) {
   var scriptOld = document.body.getElementsByTagName('script');
 
@@ -18,10 +60,9 @@ var loadAndCreate = function(addres, callback2) {
 
 
 function somethingDoWithPicturesCallback2(arr) {
-  var urlphotos = [];
-  for (var i = 0; i < arr.length; i++) {
-    urlphotos[i] = arr[i].url;
-  }
+  arr.forEach(function(picture) {
+    getPictureElement(picture, picturesContainer);
+  });
 }
 
 loadAndCreate(addr, somethingDoWithPicturesCallback2);
